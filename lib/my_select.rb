7a37498ef7f5ddcd111e508a33e730439bd66569
Harry Bloch <harryfbloch@gmail.com>
2018-09-20 18:17:21 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,14 @@
 def my_select(collection)
  if !collection.empty?
-   
+   i = 0
+   selectedCollection = []
+   while i < collection.length
+    if yield(collection[i])
+      selectedCollection << collection[i]
+    end
+ end
  else
    puts "Empty Collection"
  end
+ return selectedCollection
 end
